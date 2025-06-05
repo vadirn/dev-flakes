3c1386fe2c4e10d939c3dc284188a13ee78353a6
Vadim Kotov <vadim@vadirn.io>
2025-06-05 09:41:14 +0500

    feat: node flake, readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dev-flakes
+
+Expected folder structure to avoid copying of the project into nix storage:
+
+```
+Container folder
+├── .nix
+│   └── flake.nix
+├── Project folder (git repo)
+└── .envrc
+```
+
+`.envrc` will have
+
+```
+use flake ./.nix
+```
---
+++ node.nix
@@ -0,0 +1,41 @@
+{
+  description = "node+pnpm";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_23
+          pkgs.corepack_23
+        ];
+        shellHook = ''
+          mkdir -p "$PWD/.node/bin"
+
+          export COREPACK_HOME="$PWD/.corepack"
+          export npm_config_store_dir="$PWD/.pnpm/store"
+          export npm_config_cache_dir="$PWD/.pnpm/cache"
+          export npm_config_state_dir="$PWD/.pnpm/state"
+          export npm_config_global_dir="$PWD/.pnpm/global"
+          export npm_config_global_bin_dir="$PWD/.pnpm/global/bin"
+          export PNPM_HOME="$PWD/.pnpm/home"
+          export PATH="$npm_config_global_bin_dir:$PATH"
+          export PATH="$PWD/.node/bin:$PATH"
+          corepack enable --install-directory "$PWD/.node/bin"
+          corepack prepare pnpm@latest --activate
+          
+          # install vercel cli
+          # pnpm install -g vercel
+        '';
+      };
+    });
+}
